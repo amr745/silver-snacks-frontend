0f606fb1e73fadf5270833c6223b2e41e4316b66
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-07 22:17:17 -0700

    adds login/register component and routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
-import Index from "./pages/Index"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 const App = () => {
 
   return (
     <div className="App">
-      <Index />
+      <Header />
+      <Main />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
