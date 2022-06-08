17ae21e4bf0142a3f78e332f8e0cb5acb79785d8
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-07 21:54:22 -0700

    adds heroku link for testing in main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,25 @@
-
+import { Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const Main = () => {
+
+  const [products, setProducts] = useState(null);
+  const url = "https://silver-snacks-emporium.herokuapp.com/products";
+
+  const getProducts = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setProducts(data)
+    console.log(data)
+  };
+
+  useEffect(() => {getProducts}, []);
   return (
-    <div className="Main">
-      Main
-    </div>
+      <div className="Main">
+        Main
+      </div>
   )
 }
+
 export default Main
