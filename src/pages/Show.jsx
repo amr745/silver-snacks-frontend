bd62630398d95025312a7431f917d5ba628739d9
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-08 20:28:33 -0700

    adds show component id variables

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,8 +1,16 @@
 
 
-const Show = () => {
+const Show = ({ products }) => {
+  const { id } = useParams();
+  const product = products.find((product) => product._id === id);
+  const navigate = useNavigate();
+
   return (
-    <div>Show</div>
+    <div>
+      <h1>{product.name}</h1>
+      <img src={product.img} alt={product.name} />
+    
+    </div>
   )
 }
 export default Show
