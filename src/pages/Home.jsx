036009ba0fd4777d07277d1b87768d29e2e1f963
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-08 19:51:49 -0700

    delete index.jsx and adds getProducts function to main.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,8 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-
-const Home = () => {
-  
-  const [products, setProducts] = useState(null);
-  const url = "https://silver-snacks-emporium.herokuapp.com/products/";
-
-  const getProducts = async () => {
-    const res = await fetch(url);
-    const data = await res.json();
-    setProducts(data)
-    console.log(data)
-  };
-
-  // useEffect(() => { getProducts }, []);
+const Home = ({products}) => {
+ 
   return (
     <div>Home</div>
   )
