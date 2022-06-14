737e1f273e1a372ad7a3d7e7b4862247c2dba4af
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-13 17:15:27 -0700

    adds order subtotal and total functionality

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,6 +1,25 @@
 
 
 const order = ({cartItem}) => {
+
+    let priceArr = [];
+
+    cartItem.forEach((element) => {
+        // if(element.quantity > 1) {
+        //     priceArr.push((element.price) * element.quantity)
+        // } 
+        priceArr.push((element.price) * (element.selectedQty));
+    })
+    console.log(priceArr)
+
+    let subTotal = priceArr.reduce((a, b) => a + b);
+
+    console.log(subTotal)
+
+    let total = (subTotal * 0.07) + subTotal;    
+    
+
+
     return (
         <div>
             {cartItem.map((item) => (
@@ -8,6 +27,13 @@ const order = ({cartItem}) => {
                     <h1>{item.name}</h1>
                 </div>
             ))}
+          <div>
+            <h2>Subtotal: {subTotal.toFixed(2)}</h2>
+            <h2>Tax: 7% </h2>
+          </div>
+          <div>
+            <h2>Total: {total.toFixed(2)}</h2>
+          </div>
         </div>
     )
 }
