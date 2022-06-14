61b065240e6459f6248b66fbb3a97e42b424e04c
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-14 11:21:40 -0700

    adds css to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,11 @@ const Footer = () => {
     <footer className="footer">
       <p>SSE All Right Reserved®</p>
       <p>Silver Snacks Emporium: So good, you'll unhinge your jaw!</p>
-      <h1><FaCcVisa /></h1>
-      <h1><FaCcStripe /></h1>
-      <h1><FaBtc /></h1>
+      <div className="footer-icons">
+        <h1><FaCcVisa /></h1>
+        <h1><FaCcStripe /></h1>
+        <h1><FaBtc /></h1>
+      </div>
   </footer>
   )
 }
