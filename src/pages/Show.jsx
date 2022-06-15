804883b3dde6c6a6dbf112748063defddec5db94
Ashley Richard <ashleymrichard@gmail.com>
2022-06-14 19:04:58 -0700

    Buttons all the same

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router";
 import { useState } from "react"
 import "../App.css"
+import { Button } from "react-bootstrap";
+
 const Show = ({ products, handleClick }) => {
   const { id } = useParams();
   const product = products.find((product) => product._id === id);
@@ -12,7 +14,7 @@ const Show = ({ products, handleClick }) => {
       <div className="show-info">
       <h3 className="show-name">{product.name}</h3>
       <div className="show-price"><h4>${(product.price).toFixed(2)}</h4></div>
-      <button className="show-button" onClick={() => handleClick(product)}>Buy Now</button>
+      <Button className="show-button" onClick={() => handleClick(product)}>Buy Now</Button>
       <div className="show-qnt"><h4>Only {product.quantity} items in stock!</h4></div>
       <h5 className="show-des">{product.description}</h5>
       </div>
