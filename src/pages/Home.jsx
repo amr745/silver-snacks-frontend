625942a2bf78967ee44507f6f0f105c6d6012395
Javier Delgado <vanillajaviscript@gmail.com>
2022-06-15 12:00:23 -0700

    adds fix to home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Show from "./Show";
 import { Card, Button, Row, Col } from "react-bootstrap";
+import { FaGratipay } from "react-icons/fa";
 const Home = ({ products }) => {
 
 
@@ -14,7 +15,7 @@ const Home = ({ products }) => {
           <Col className="mb-3 mt-3">
             <div className="product-data" key={product._id}>
 
-              <Card style={{ width: '18rem', margin: 'auto', backgroundColor: "rgb(80 80 85)", borderRadius: "8px" }}>
+              <Card style={{ width: '18rem', margin: 'auto', backgroundColor: "rgb(80 80 85)", borderRadius: "8px", boxShadow: "4px 8px 9px gray" }}>
                 <Link style={{ textDecoration: "none" }} to={`/products/${product._id}`}> <Card.Img className="product-card-img" style={{ height: "286" }} variant="top" src={product.img} /></Link>
 
                 <Card.Body>
