9da9b6a99672414bdfe66f2b0ce5ccc4ca517ed0
Ashley Richard <ashleymrichard@gmail.com>
2022-09-26 19:37:55 -0700

    adds HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
